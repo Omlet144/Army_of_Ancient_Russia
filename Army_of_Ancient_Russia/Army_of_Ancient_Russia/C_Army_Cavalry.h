d06ed9b2d6e590a85c589aa1a3df935f8ea94b8a
Omlet144 <alexblack144@gmail.com>
2021-08-13 11:10:23 +0300

    add: - namespace army:   - inner_cavalry   - inner_fleet   - inner_infantry

--- Army_of_Ancient_Russia/Army_of_Ancient_Russia/C_Army_Cavalry.h
+++ Army_of_Ancient_Russia/Army_of_Ancient_Russia/C_Army_Cavalry.h
@@ -1,9 +1,15 @@
 #pragma once
-#include "C_Army.h"
 #include <iostream>
-class C_Army_Cavalry :
-    public C_Army
-{
-    virtual void amount(int number_of_troops) override { std::cout << "Cavalry =" << number_of_troops << "\n"; }
+#include "C_Army.h"
+namespace army {
+	namespace inner_cavalry
+	{
+		class C_Army_Cavalry :
+			public C_Army
+		{
+			virtual void amount(int number_of_troops) override { std::cout << "Cavalry =" << number_of_troops << "\n"; }
+		};
+	}
 };
+
 
--- Army_of_Ancient_Russia/Army_of_Ancient_Russia/C_Army_Infantry.h
+++ Army_of_Ancient_Russia/Army_of_Ancient_Russia/C_Army_Infantry.h
@@ -1,9 +1,14 @@
 #pragma once
 #include "C_Army.h"
 #include <iostream>
-class C_Army_Infantry :
-    public C_Army
-{
-    virtual void amount(int number_of_troops) override { std::cout << "Infantry =" <<number_of_troops<< "\n"; }
+namespace army {
+	namespace inner_infantry
+	{
+		class C_Army_Infantry :
+			public C_Army
+		{
+			virtual void amount(int number_of_troops) override { std::cout << "Infantry =" << number_of_troops << "\n"; }
+		};
+	}
 };
 
