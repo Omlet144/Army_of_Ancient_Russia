6ff01194378319ceea090950916ad992653b1d52
Omlet144 <alexblack144@gmail.com>
2021-08-13 11:14:54 +0300

    add: - namespace armaments:   - inner_throwingmachines   - inner_protective   - inner_offensive

--- Army_of_Ancient_Russia/Army_of_Ancient_Russia/C_Armaments_Protective.h
+++ Army_of_Ancient_Russia/Army_of_Ancient_Russia/C_Armaments_Protective.h
@@ -1,10 +1,16 @@
 #pragma once
 #include "C_Armaments.h"
 #include <iostream>
-class C_Armaments_Protective :
-    public C_Armaments
+namespace armaments
 {
-    virtual void attack(int attack_armaments)override = 0;
-    virtual void breack(int break_armaments) override { std::cout << "Break of Protective = "<<break_armaments << "\n"; }
+    namespace inner_protective
+    {
+        class C_Armaments_Protective :
+            public C_Armaments
+        {
+            virtual void attack(int attack_armaments)override = 0;
+            virtual void breack(int break_armaments) override { std::cout << "Break of Protective = " << break_armaments << "\n"; }
+        };
+    }
 };
 
